82aedd17759b02e1ce09c992a6030a401a931c31
VladVes <veselov.workflow@gmail.com>
2020-01-11 17:07:51 +0700

    fix

--- main/index.js
+++ main/index.js
@@ -1,9 +1,11 @@
 import alert from './utils.js';
 
+import { fib } from 'my-lib/fib.js';
+
 function main() {
   const appElem = document.getElementById('app');
   appElem.innerHTML = '<h1>Monorepo go!</h1>';
-  alert('It works!')
+  alert('It works! ' + fib(10));
 }
 
 window.onload = main;
