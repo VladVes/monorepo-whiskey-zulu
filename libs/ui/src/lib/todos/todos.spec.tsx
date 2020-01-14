0d8e31c4b61b7d45840ad06a1f16756cf7545d6a
VladVes <veselov.workflow@gmail.com>
2020-01-14 20:53:35 +0700

    update

--- libs/ui/src/lib/todos/todos.spec.tsx
+++ libs/ui/src/lib/todos/todos.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Todo } from '@monorepo-whiskey-zulu/data';
 
 import Todos from './todos';
 
--- libs/ui/src/lib/todos/todos.tsx
+++ libs/ui/src/lib/todos/todos.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { Todo } from '@monorepo-whiskey-zulu/data';
 
-export const Todos = (props: { todos: Todo[] }) => {
-  return (
-    <ul>
-      {props.todos.map(t => (
-        <li className={'todo'}>{t.title}</li>
-      ))}
-    </ul>
-  );
-};
+export const Todos = (props: { todos: Todo[] }) =>
+  props.todos.map(t => <li className={'todo'}>{t.title} !!</li>);
+
 
 export default Todos;
